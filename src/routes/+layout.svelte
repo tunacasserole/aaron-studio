ee5ef4495f9b722551ca99b56ef3bb7b053bd5ab
Aaron Henderson <aaron@mashbot.com>
2023-06-27 18:03:04 -0600

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import '../app.css';
+
+	/** @type {import('./$types').LayoutServerData} */
+	export let data;
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>developed by www.solidstack.io</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
